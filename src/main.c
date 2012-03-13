1d2319a42742fcca642f2899f7cff1da0f006211
Wilson Kazuo Mizutani <kazuo256@gmail.com>
2012-03-13 14:44:59 -0300

    Code style adjustments.
    We have no code style defined yet, though.

--- src/main.c
+++ src/main.c
@@ -4,25 +4,25 @@
 #include <pthread.h>
 #include <unistd.h>
 
-void *thread_function(void *arg) {
+void *thread_function (void *arg) {
   int i;
-  for ( i=0; i<20; i++ ) {
+  for (i=0; i<20; i++) {
     printf("Thread says hi!\n");
     sleep(1);
   }
   return NULL;
 }
 
-int main(void) {
+int main (void) {
 
   pthread_t mythread;
       
-  if ( pthread_create( &mythread, NULL, thread_function, NULL) ) {
+  if (pthread_create(&mythread, NULL, thread_function, NULL)) {
     printf("error creating thread.");
     return EXIT_FAILURE;
   }
 
-  if ( pthread_join ( mythread, NULL ) ) {
+  if (pthread_join(mythread, NULL)) {
     printf("error joining thread.");
     return EXIT_FAILURE;
   }
